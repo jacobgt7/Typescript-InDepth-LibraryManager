9339ee8cbc917df214a4342a9a1333c7aea32c76
Jacob Thomas <jacgretho@gmail.com>
2023-04-13 16:44:11 -0500

    WIP

--- app.js
+++ app.js
@@ -2,19 +2,14 @@ function GetAllBooks() {
     let books = [
         { title: 'Ulysses', author: 'James Joyce', available: true },
         { title: 'A Farewell to Arms', author: 'Ernest Hemingway', available: false },
-        { title: 'I Know Why the Caged Bird Sings', author: 'Maya Angelou', available: true },
-        { title: 'Moby Dick', author: 'Herman Melville', available: true }
+        { title: 'I Know Why the Caged Bird Sings', author: 'Maya Angelou', available: true }
     ];
-
     return books;
 }
-
-function LogFirstAvailable(books): void {
-    let numberOfBooks: number = books.length;
-    let firstAvailable: string = '';
-
+function LogFirstAvailable(books) {
+    let numberOfBooks = books.length;
+    let firstAvailable = '';
     for (let currentBook of books) {
-
         if (currentBook.available) {
             firstAvailable = currentBook.title;
             break;
@@ -23,6 +18,5 @@ function LogFirstAvailable(books): void {
     console.log('Total Books: ' + numberOfBooks);
     console.log('First Available: ' + firstAvailable);
 }
-
 const allBooks = GetAllBooks();
 LogFirstAvailable(allBooks);
